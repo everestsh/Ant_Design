30359f41599d97e35f1d75521f0cf98a0ead4488
Mingjun Geng <sooofeve@gmail.com>
2022-05-28 12:23:46 -0700

    foldname app : TimePicker Component

--- foldername/src/App.js
+++ foldername/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import "antd/dist/antd.css";
+import { TimePicker } from 'antd';
+ 
+export default function App() {
+ 
+return (
+    <div style={{
+    display: 'block', width: 700, padding: 30
+    }}>
+    <h4>ReactJS Ant-Design TimePicker Component</h4>
+    <>
+        <TimePicker onChange={(time) => console.log(time)} />,
+    </>
     </div>
-  );
+);
 }
-
-export default App;
